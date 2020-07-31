42a069c82601e2fc3c5e20442ebd8b0c6aceb23c
Janiek Buysrogge <janiek.buysrogge@gmail.com>
2020-07-31 13:07:18 +0200

    Add project files.

---
+++ mt-amq-test/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Threading.Tasks;
+using MassTransit;
+using MassTransit.ActiveMqTransport;
+
+namespace mt_amq_test
+{
+    public class Message
+    {
+        public string Text { get; set; }
+    }
+
+    class Program
+    {
+        public static async Task Main()
+        {
+            Console.WriteLine("Wait a bit for the bus to get ready");
+
+            await Task.Delay(5000);
+
+            Console.WriteLine("Configuring...");
+
+            var bus = Bus.Factory.CreateUsingActiveMq(sbc =>
+            {
+                sbc.Host("broker", h =>
+                {
+                    //h.UseSsl();
+                    h.Username("admin");
+                    h.Password("admin");
+                });
+
+                sbc.ReceiveEndpoint("test-queue", ep =>
+                {
+                    ep.Handler<Message>(context =>
+                    {
+                        return Console.Out.WriteLineAsync($"Received: {context.Message.Text}");
+                    });
+                });
+            });
+
+            Console.WriteLine("Starting...");
+            await bus.StartAsync();
+
+            Console.WriteLine("Publishing message...");
+            await bus.Publish(new Message { Text = "Hi" });
+
+            Console.WriteLine("Waiting for message to arrive...");
+            await Task.Delay(15000);
+
+            await bus.StopAsync();
+        }
+    }
+}
